357122f0f97a8515146209531bdaead99497e2f5
Vladislav Evstigneev <vladislav.evstigneev@carrier.com>
2024-02-06 19:07:48 +0400

    add rabbit mq set up

--- src/config/vars.ts
+++ src/config/vars.ts
@@ -3,7 +3,10 @@ import { parseString } from '../utils/parse/parseString.ts';
 
 export const PORT = parseNumber(process.env.PORT, 8000);
 export const DOMAIN_URL = parseString(process.env.DOMAIN_URL, 'http://localhost:3000');
-export const COOKIE_SECRET = parseString(process.env.DOMAIN_URL, 'cookieSecret');
+export const COOKIE_SECRET = parseString(process.env.COOKIE_SECRET, 'cookieSecret');
+export const BROKER_PORT = parseNumber(process.env.BROKER_PORT, 5672);
+export const BROKER_HOST = parseString(process.env.BROKER_HOST, 'localhost');
+export const BROKER_PROTOCOL = parseString(process.env.BROKER_PROTOCOL, 'amqp');
 
 export default {
   common: {
@@ -13,5 +16,11 @@ export default {
   server: {
     port: PORT || 8000,
   },
-  connections: {},
+  connections: {
+    broker: {
+      host: BROKER_HOST,
+      port: BROKER_PORT,
+      protocol: BROKER_PROTOCOL,
+    },
+  },
 };
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,16 @@ import express, { Express } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
-import { logger, loggerInstance } from './logger/index.ts';
-import { appConfig } from './config/Config.ts';
-import apiV1Router from './api/v1/index.ts';
+import { logger, loggerInstance } from '../logger/index.ts';
+import { appConfig } from '../config/Config.ts';
+import apiV1Router from '../api/v1/index.ts';
+import { testEventConsumer } from '../broker/consumers';
+import { Broker } from '../broker';
+import { publishTestEvent } from '../broker/publishers';
 
 const domainUrl = appConfig.get('common.domainUrl');
 const cookieSecret = appConfig.get('common.cookieSecret');
+const brokerConnection = appConfig.get('connections.broker');
 const port = appConfig.get('server.port');
 
 export class Server {
@@ -34,8 +38,19 @@ export class Server {
     this.app.use('/v1', apiV1Router);
   }
 
-  start() {
+  async start(port: number) {
     this.catchUncaughtException();
+
+    if (brokerConnection) {
+      await Broker.init(`${brokerConnection.protocol}://${brokerConnection.host}:${brokerConnection.port}`);
+
+      await testEventConsumer();
+      const data = await publishTestEvent({
+        hello: 'world',
+      });
+
+      console.log(data);
+    }
 
     this.server.listen(port, () => {
       logger.info(`Server is running at http://localhost:${port}`);
