bfffe62b69df504906dbfacb22cdd76e65c0662c
Vladislav Evstigneev <vladislav.evstigneev@carrier.com>
2024-10-27 18:35:54 +0400

    feat: update packages and eslint configuration

--- src/config/vars.ts
+++ src/config/vars.ts
@@ -1,12 +1,11 @@
 import { parseNumber } from '../utils/parse/parseNumber.ts';
-import { parseString } from '../utils/parse/parseString.ts';
 
 export const PORT = parseNumber(process.env.PORT, 8000);
-export const DOMAIN_URL = parseString(process.env.DOMAIN_URL, 'http://localhost:3000');
-export const COOKIE_SECRET = parseString(process.env.COOKIE_SECRET, 'cookieSecret');
+export const DOMAIN_URL = process.env.DOMAIN_URL ?? 'http://localhost:3000';
+export const COOKIE_SECRET = process.env.COOKIE_SECRET ?? 'cookieSecret';
 export const BROKER_PORT = parseNumber(process.env.BROKER_PORT, 5672);
-export const BROKER_HOST = parseString(process.env.BROKER_HOST, 'localhost');
-export const BROKER_PROTOCOL = parseString(process.env.BROKER_PROTOCOL, 'amqp');
+export const BROKER_HOST = process.env.BROKER_HOST ?? 'localhost';
+export const BROKER_PROTOCOL = process.env.BROKER_PROTOCOL ?? 'amqp';
 
 export default {
   common: {
@@ -14,7 +13,7 @@ export default {
     cookieSecret: COOKIE_SECRET,
   },
   server: {
-    port: PORT || 8000,
+    port: PORT,
   },
   connections: {
     broker: {
